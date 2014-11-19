8949d90bb6d147560551f48c6504d19c224a0830
Manmeet Saluja <manmeet.saluja94@gmail.com>
2014-11-19 12:15:18 -0800

    built the sample node app that responds with ack to a ping

---
+++ app.js
@@ -0,0 +1,26 @@
+
+var restify = require('restify');
+var server = restify.createServer();
+server.use(restify.bodyParser());
+server.use(restify.gzipResponse());
+ 
+
+server.post({ path: '/ping'}, pingCreation);
+
+function pingCreation(request, result, next) {
+  console.log(request.body, typeof request.body);
+  console.log(request.body['handshake']);
+  if(request.body.handshake == "ping") {
+    result.send({"handshake":"ack"});
+  }
+  else {
+    result.send({ "handshake":"notack" });
+  }
+  
+  return next();
+}
+
+server.listen(8080, function() {
+  console.log('%s listening at %s', server.name, server.url);
+});
+
